959782a10ebd3afadea8881bc0d5484177d82376
Hugo Cueva <hugo.cueva@owners-link.com>
2020-10-20 20:00:48 -0500

    API routes / implementation

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -43,26 +43,90 @@ exports.findAll = (req, res) => {
 
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
-
+    const id = req.params.id; 
+    Tutorial.findById(id)
+        .then(data => {
+            if(!data)
+                res.status(404).send({message: `Didn't find Tutorial with id ${id}`}); 
+            else res.send(data);
+        })
+        .catch(err => {
+            res 
+            .status(500)
+            .send({message: `Error retrieving Tutorial with id ${id}`}); 
+        }); 
 }; 
 
 
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {
+    if(!req.body){
+        return res.status(400).send({
+            nessage: `Data to update can't be empty`
+        }); 
+    }
 
+    const id = req.params.id; 
+
+    Tutorial.findByIdAndUpdate(id, req.body, {useFindAndModify:false})
+        .then(data => {
+            if(!data){
+                res.status(404).send({
+                    message: `Cant't update Tutorial with id=${id}. Maybe tutorial was not found!`
+                }); 
+            }else res.send({ message: `Tutorial was updated successfully.`}); 
+
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Error updating Tutorial with id=${id}`
+            })
+        });
 }; 
 
 // Delete a Tutorial by the id in the request
 exports.delete = (req, res) => {
-
+    const id = req.params.id; 
+    Tutorial.findByIdAndRemove(id)
+        .then(data => {
+            if(!data){
+                res.status(404).send({
+                    message: `Can't delete Tutorial with id=${id}. Maybe tutorial was not found`
+                });
+            }else res.send({ message: `Tutorial was deleted successfully!`}); 
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Could not delete Tutorial with id=${id}`
+            });
+        })
 }; 
 
 // Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
-
+    Tutorial.deleteMany({})
+        .then(data => { 
+            res.send({
+                message: `${data.deletedCount} Tutorials were deleted successfully!`
+            })
+        })
+        .catch(err =>{ 
+            req.status(500).send({ 
+                message: err.message || `Some error ocurred while removing all tutorials`
+            })
+        })
 }; 
 
 // Find all published Tutorials
 exports.findAllPublished = (req, res) => {
-
+    Tutorial.find({ published:true})
+        .then(data =>{
+            res.send(data); 
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message: 
+                    err.message || `Some error ocurred while retrieving tutorials`
+            })
+        }); 
 }; 
